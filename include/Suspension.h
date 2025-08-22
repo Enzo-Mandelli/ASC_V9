5d2a8f5a26a1ba86967572f026279e98649f2212
Enzo-Mandelli <enzo.contato.me@gmail.com>
2025-08-22 00:32:28 -0300

    adicao das funcoes que movem os servos

--- include/Suspension.h
+++ include/Suspension.h
@@ -2,24 +2,13 @@
 class Suspension{
     public:
         Suspension(Servo fr, Servo fl, Servo rr, Servo rl);
-        Servo servoFrontLeft;
-        Servo servoFrontRight;
-        Servo servoRearRight;
-        Servo servoRearLeft;
-        int max;
-        int min;
-        void setZero(int zero);
-        void setMax(int max);
-        void setAngleRight(int angle);
-        void setAngleLeft(int angle);
-        void setAngleFront(int angle);
-        void setAngleRear(int angle);
-        void testing();
-        Legs(Servo* ptr_ServoFL, Servo* ptr_ServoFR, Servo* ptr_ServoRL, Servo* ptr_ServoRR){
+
+        
+        Suspension(Servo* ptr_ServoFL, Servo* ptr_ServoFR, Servo* ptr_ServoRL, Servo* ptr_ServoRR){
             servoFrontLeft = *ptr_ServoFL;
             servoFrontRight = *ptr_ServoFR;
             servoRearLeft = *ptr_ServoRL;
-            servoRearRight = *ptr_Servo p00000000000000000000000pppç~;.l][´´´´´´´´´´p[]];
+            servoRearRight = *ptr_ServoRR;
         }
         void setPosInativo();
         void setPosAtivo();
@@ -27,5 +16,16 @@ class Suspension{
         void moveRightSide(int angulo);
         void moveFrontSide(int angulo);
         void moveBackSide(int angulo);
-
+        void moveLeftSideMM(int medida);
+        void moveRightSideMM(int medida);
+        void moveFrontSideMM(int medida);
+        void moveBackSideMM(int medida);
+        int max;
+        int min;
+        void setZero(int zero);
+        void setMax(int max);
+        Servo servoFrontLeft;
+        Servo servoFrontRight;
+        Servo servoRearRight;
+        Servo servoRearLeft;
 };
--- src/Suspension.cpp
+++ src/Suspension.cpp
@@ -1,44 +1,71 @@
 #include <ESP32Servo.h>
-#ifndef Legs.h
-#include <Legs.h>
+#ifndef Suspension.h
+#include <Suspension.h>
 #endif
 void move(Servo &servo, int angulo, bool invert);
-int lowerAngle = 0;
-int biggestAngle = 180;
-int restAngle = 0;
-int activeAngle = 90;
-void Legs :: setPosInativo(){
-    move(servoFrontLeft, restAngle, false);
-    move(servoFrontRight, restAngle, false);
-    move(servoRearLeft, restAngle, false);
-    move(servoRearRight, restAngle, false);
+int lowerAngle = 0;//lowest possible value
+int biggestAngle = 180;//highest possible value
+int restAngle = 0;//lowest usable value
+int activeAngle = 90;//highest usable value
+void Suspension :: setMax(int max){
+    activeAngle = max;
 }
 
-void Legs :: setPosAtivo(){
-    move(servoFrontLeft, activeAngle, false);
-    move(servoFrontRight, activeAngle, false);
-    move(servoRearLeft, activeAngle, false);
-    move(servoRearRight, activeAngle, false);
+void Suspension :: setZero(int min){
+    activeAngle = min;
 }
 
-void Legs :: moveBackSide(int angulo){
-    move(servoRearLeft, angulo, false);
-    move(servoRearRight, angulo, false);
+void Suspension :: setPosInativo(){
+    servoFrontLeft.write(restAngle);
+    servoFrontRight.write(restAngle);
+    servoRearLeft.write(restAngle);
+    servoRearRight.write(restAngle);
 }
 
-void Legs :: moveFrontSide(int angulo){
-    move(servoFrontLeft, angulo, false);
-    move(servoFrontRight, angulo, false);
+void Suspension :: setPosAtivo(){
+    servoFrontLeft.write(activeAngle);
+    servoFrontRight.write(activeAngle);
+    servoRearLeft.write(activeAngle);
+    servoRearRight.write(activeAngle);
 }
 
-void Legs :: moveLeftSide(int angulo){
-    move(servoFrontLeft, angulo, false);
-    move(servoRearLeft, angulo, false);
+void Suspension :: moveBackSide(int angulo){
+    servoRearLeft.write(angulo);
+    servoRearRight.write(angulo);
 }
 
-void Legs :: moveRightSide(int angulo){
-    move(servoFrontRight, angulo, false);
-    move(servoRearRight, angulo, false);
+void Suspension :: moveFrontSide(int angulo){
+    servoFrontLeft.write(angulo);
+    servoFrontRight.write(angulo);
 }
 
-void move(Servo &servo, 
+void Suspension :: moveLeftSide(int angulo){
+    servoFrontLeft.write(activeAngle);
+    servoRearLeft.write(activeAngle);
+}
+
+void Suspension :: moveRightSide(int angulo){
+    servoFrontRight.write(activeAngle);
+    servoRearRight.write(activeAngle);
+}
+
+void move(Servo &servo, int angulo) {
+    servo.write(angulo);
+}
+
+void Suspension :: moveBackSideMM(int angulo){
+    //tem que elaborar a aritmetica disso aqui
+}
+
+void Suspension :: moveFrontSideMM(int angulo){
+    //tem que elaborar a aritmetica disso aqui
+}
+
+void Suspension :: moveLeftSideMM(int angulo){
+    //tem que elaborar a aritmetica disso aqui
+}
+
+void Suspension :: moveRightSideMM(int angulo){
+    //tem que elaborar a aritmetica disso aqui
+}
+
